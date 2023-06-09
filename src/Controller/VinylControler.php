ab96afae35b3f4c4618ffd9dfa7b6e5e1aaee0fc
bawarzec <bartlomiejwarzecha2@gmail.com>
2023-06-09 12:06:46 +0200

    Add route with web browser param

--- src/Controller/VinylControler.php
+++ src/Controller/VinylControler.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use function Symfony\Component\String\u;
+
 
 class VinylControler
 {
@@ -11,6 +13,18 @@ class VinylControler
     public function homepage() : Response
     {
         return new Response('Title: PB and Jams');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null) : Response
+    {
+
+        if($slug) {
+            $title = u(str_replace('-', '', $slug))->title(true);
+        }else{
+            $title = 'All Genres';
+        }
+        return new Response('Genre = '.$title );
     }
 
 }
